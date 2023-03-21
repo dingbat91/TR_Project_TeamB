d306b67e21a6021a4f54ef20ef5e51753e50c70e
Matthew Hanson <dingbat91@gmail.com>
2023-03-21 19:25:06 +0000

    Initial movie page

---
+++ src/types/movie.ts
@@ -0,0 +1,51 @@
+export interface MovieDetails {
+	adult: boolean;
+	backdrop_path: string | null;
+	belongs_to_collection: object | null;
+	budget: number;
+	genres: Genre[];
+	homepage: string | null;
+	id: number;
+	imdb_id: string | null;
+	original_language: string;
+	original_title: string;
+	overview: string | null;
+	popularity: number;
+	poster_path: string | null;
+	production_companies: ProductionCompany[];
+	production_countries: ProductionCountry[];
+	release_date?: Date;
+	revenue?: number;
+	runtime?: number | null;
+	spoken_languages: SpokenLanguage[];
+	status?:
+		| "Rumored"
+		| "Planned"
+		| "In Production"
+		| "Post Production"
+		| "Released"
+		| "Canceled";
+	tagline?: string | null;
+}
+
+interface Genre {
+	id: number;
+	name: string;
+}
+
+interface ProductionCompany {
+	name: string;
+	id: number;
+	logo_path?: string;
+	origin_country?: string;
+}
+
+interface ProductionCountry {
+	iso_3166_1?: string;
+	name?: string;
+}
+
+interface SpokenLanguage {
+	iso_639_1?: string;
+	name?: string;
+}
