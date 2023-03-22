c19feee629634c56ddea7876a9eea62de3f384c2
Matthew Hanson <dingbat91@gmail.com>
2023-03-22 18:50:03 +0000

    added handler for John Wick Test Case

--- src/scripts/fetch/testing/mswConfig/handlers.ts
+++ src/scripts/fetch/testing/mswConfig/handlers.ts
@@ -95,6 +95,109 @@ export const handlers = [
 			})
 		);
 	}),
+	rest.get("https://api.themoviedb.org/3/movie/603692", (req, res, ctx) => {
+		return res(
+			ctx.status(200),
+			ctx.json({
+				adult: false,
+				backdrop_path: "/h8gHn0OzBoaefsYseUByqsmEDMY.jpg",
+				belongs_to_collection: {
+					id: 404609,
+					name: "John Wick Collection",
+					poster_path: "/xUidyvYFsbbuExifLkslpcd8SMc.jpg",
+					backdrop_path: "/fSwYa5q2xRkBoOOjueLpkLf3N1m.jpg",
+				},
+				budget: 90000000,
+				genres: [
+					{
+						id: 28,
+						name: "Action",
+					},
+					{
+						id: 53,
+						name: "Thriller",
+					},
+					{
+						id: 80,
+						name: "Crime",
+					},
+				],
+				homepage: "https://johnwick.movie",
+				id: 603692,
+				imdb_id: "tt10366206",
+				original_language: "en",
+				original_title: "John Wick: Chapter 4",
+				overview:
+					"With the price on his head ever increasing, John Wick uncovers a path to defeating The High Table. But before he can earn his freedom, Wick must face off against a new enemy with powerful alliances across the globe and forces that turn old friends into foes.",
+				popularity: 950.477,
+				poster_path: "/vZloFAK7NmvMGKE7VkF5UHaz0I.jpg",
+				production_companies: [
+					{
+						id: 3528,
+						logo_path: "/cCzCClIzIh81Fa79hpW5nXoUsHK.png",
+						name: "Thunder Road",
+						origin_country: "US",
+					},
+					{
+						id: 23008,
+						logo_path: "/5SarYupipdiejsEqUkwu1SpYfru.png",
+						name: "87Eleven",
+						origin_country: "US",
+					},
+					{
+						id: 1632,
+						logo_path: "/cisLn1YAUuptXVBa0xjq7ST9cH0.png",
+						name: "Lionsgate",
+						origin_country: "US",
+					},
+					{
+						id: 491,
+						logo_path: "/rUp0lLKa1pr4UsPm8fgzmnNGxtq.png",
+						name: "Summit Entertainment",
+						origin_country: "US",
+					},
+					{
+						id: 195559,
+						logo_path: "/fmh0UMCOa7JsDr6UhLESPePc3PE.png",
+						name: "El-Torky Art Production",
+						origin_country: "",
+					},
+				],
+				production_countries: [
+					{
+						iso_3166_1: "US",
+						name: "United States of America",
+					},
+				],
+				release_date: "2023-03-22",
+				revenue: 0,
+				runtime: 169,
+				spoken_languages: [
+					{
+						english_name: "English",
+						iso_639_1: "en",
+						name: "English",
+					},
+					{
+						english_name: "French",
+						iso_639_1: "fr",
+						name: "Français",
+					},
+					{
+						english_name: "Japanese",
+						iso_639_1: "ja",
+						name: "日本語",
+					},
+				],
+				status: "Released",
+				tagline: "No way back, one way out.",
+				title: "John Wick: Chapter 4",
+				video: false,
+				vote_average: 8.7,
+				vote_count: 34,
+			})
+		);
+	}),
 	rest.get("https://api.themoviedb.org/3/person/287", (req, res, ctx) => {
 		ctx.status(200);
 		ctx.json({
