f54a1d87ec678142bf5fcd0529560ef07f0ddef0
Matthew Hanson <dingbat91@gmail.com>
2023-03-22 18:38:15 +0000

    Adjusted page to work with React Router

--- src/pages/movie/MoviePage.test.tsx
+++ src/pages/movie/MoviePage.test.tsx
@@ -1,14 +1,31 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import { MoviePage } from "./MoviePage";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
 
 describe(" Render Tests", () => {
 	test("Page Renders", async () => {
-		render(<MoviePage />);
+		const movieID = 550;
+
+		render(
+			<MemoryRouter initialEntries={[`/movies/${movieID}`]}>
+				<Routes>
+					<Route path='/movies/:movieID/*' element={<MoviePage />} />
+				</Routes>
+			</MemoryRouter>
+		);
 		expect(await screen.findByText("Fight Club")).toBeInTheDocument();
 	});
 
 	test("Page Maps Genre's correctly", async () => {
-		render(<MoviePage />);
+		const movieID = 550;
+		render(
+			<MemoryRouter initialEntries={[`/movies/${movieID}`]}>
+				<Routes>
+					<Route path='/movies/:movieID/*' element={<MoviePage />} />
+				</Routes>
+			</MemoryRouter>
+		);
+
 		expect(await screen.findByText("Drama")).toBeInTheDocument();
 	});
 });
