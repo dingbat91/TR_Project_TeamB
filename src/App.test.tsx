8301d7186052a955a4a8644288ac877f167ac9d8
mrunalshri <mrunalyetekar@gmail.com>
2023-03-22 23:42:08 +0000

    removed unwanted tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,18 +1,9 @@
 import React from "react";
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import App from "./App";
 
 test("renders Team PopCorn header", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Team PopCorn/i);
-  expect(linkElement).toBeInTheDocument();
-});
-test("render select dropdown value correctly", async () => {
-  render(<App />);
-  const selectInputElement = screen.getByLabelText(/select-options/);
-
-  expect(selectInputElement).toBeInTheDocument();
-  fireEvent.change(selectInputElement, { target: { value: "12" } });
-  const selectElement = await screen.findByLabelText(/select-options/);
-  expect(selectElement).toHaveValue("12");
+  const { container } = render(<App />);
+  // eslint-disable-next-line testing-library/no-node-access
+  expect(container.firstChild).toHaveClass("App");
 });
