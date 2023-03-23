3a3ea30e7ad187189a19e1a27249f6c068274cd8
Matthew Hanson <dingbat91@gmail.com>
2023-03-23 16:43:05 +0000

    Adjusted moviecard to new APIFetch
    Co-authored-by: Jon <Jon-techReturner@users.noreply.github.com>

--- src/card/movie_card.tsx
+++ src/card/movie_card.tsx
@@ -1,20 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { APIFetch } from "../scripts/fetch/fetch";
 import { MovieDetails } from "../types/movie";
 
 interface MovieCardInterface {
-	movieid: number
+	movieid: number;
 }
 
+export const MovieData: React.FC<MovieCardInterface> = ({ movieid }) => {
+	const [moviedata, setMovieData] = useState<MovieDetails>();
+	const movieID = { movieid };
 
-export const MovieData:React.FC<MovieCardInterface> = ({movieid}) => {
-  const [moviedata, setMovieData] = useState<MovieDetails>();
-  const movieID={movieid};
-  
-  useEffect(() => {
+	useEffect(() => {
 		const datafetch = async () => {
 			if (process.env.REACT_APP_APIKEY) {
-				const data = await APIFetch(`/movie/${movieID}`, process.env.REACT_APP_APIKEY);
+				const data = await APIFetch(
+					`${process.env.REACT_APP_BASE_URL},/movie/${movieID}`
+				);
 				setMovieData(data);
 			} else {
 				throw new Error("Missing API KEY");
@@ -23,13 +24,17 @@ export const MovieData:React.FC<MovieCardInterface> = ({movieid}) => {
 		datafetch();
 	}, []);
 
-  const imagesrc = `https://image.tmdb.org/t/p/original${moviedata?.poster_path}`;
+	const imagesrc = `https://image.tmdb.org/t/p/original${moviedata?.poster_path}`;
 
-  return (
-    <article className="card">
-      <img className="card__img" data-id = {moviedata?.id} src={imagesrc} alt={moviedata?.original_title} />
-      <h3 className="card__title">{moviedata?.original_title}</h3>
-    </article>
-  );
+	return (
+		<article className='card'>
+			<img
+				className='card__img'
+				data-id={moviedata?.id}
+				src={imagesrc}
+				alt={moviedata?.original_title}
+			/>
+			<h3 className='card__title'>{moviedata?.original_title}</h3>
+		</article>
+	);
 };
-
