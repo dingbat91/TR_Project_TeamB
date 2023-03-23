cf1d09266cdc63c2dccef612d27d56783282b3ff
Matthew Hanson <dingbat91@gmail.com>
2023-03-23 20:01:08 +0000

    Basic Card Row made

--- src/Components/cardRow/CardRow.tsx
+++ src/Components/cardRow/CardRow.tsx
@@ -1,5 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { MovieData } from "../../card/movie_card";
+import { APIFetch } from "../../scripts/fetch/fetch";
+import { TopRatedMovie } from "../../types/movie";
+import Dropdown from "../Dropdown/Dropdown";
+import "./CardRow.css";
 
-export const CardRow = () => {
-	return <div>CardRow</div>;
+interface CardRowInterface {
+	movieIDs: number[];
+}
+
+export const CardRow: React.FC = () => {
+	const [movieIDs, setMovieIDs] = useState<number[]>([]);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const data = (await APIFetch("/movie/top_rated")) as TopRatedMovie;
+			const IDs = data.results.map((result) => result.id);
+			setMovieIDs(IDs);
+		};
+		fetchData();
+	}, []);
+
+	return (
+		<div className='CardRow'>
+			{movieIDs.map((id) => (
+				<MovieData movieid={id} />
+			))}
+		</div>
+	);
 };
