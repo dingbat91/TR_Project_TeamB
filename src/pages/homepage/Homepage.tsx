92ffa87a5f9ffbb31675d023ad5e06e1e4fd59c5
mrunalshri <mrunalyetekar@gmail.com>
2023-03-25 12:30:03 +0000

    fix: updated home component

--- src/pages/homepage/Homepage.tsx
+++ src/pages/homepage/Homepage.tsx
@@ -1,72 +1,82 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import "./Homepage.css";
 import Card from "../../components/card/card";
 import Search from "../../components/Search/search";
 import { getMoviesByKeyword } from "../../scripts/utils";
 import { MovieDetails } from "../../types/movie";
+import debounce from "lodash.debounce";
 
 export function Homepage() {
-	//TODO Implement https://api.themoviedb.org/3/genre/movie/list?api_key=API_KEY&language=en-US
-	// to get all genres
-	// const [genres, setGenres] = useState<Array<any>>([
-	//   {
-	//     id: 28,
-	//     name: "Action",
-	//   },
-	//   {
-	//     id: 12,
-	//     name: "Adventure",
-	//   },
-	//   {
-	//     id: 16,
-	//     name: "Animation",
-	//   },
-	//   {
-	//     id: 35,
-	//     name: "Comedy",
-	//   },
-	//   {
-	//     id: 80,
-	//     name: "Crime",
-	//   },
-	// ]);
-	// const [selectedGenreID, setSelectedGenreID] = useState<string>(genres[0].id);
+  //TODO Implement https://api.themoviedb.org/3/genre/movie/list?api_key=API_KEY&language=en-US
+  // to get all genres
+  // const [genres, setGenres] = useState<Array<any>>([
+  //   {
+  //     id: 28,
+  //     name: "Action",
+  //   },
+  //   {
+  //     id: 12,
+  //     name: "Adventure",
+  //   },
+  //   {
+  //     id: 16,
+  //     name: "Animation",
+  //   },
+  //   {
+  //     id: 35,
+  //     name: "Comedy",
+  //   },
+  //   {
+  //     id: 80,
+  //     name: "Crime",
+  //   },
+  // ]);
+  // const [selectedGenreID, setSelectedGenreID] = useState<string>(genres[0].id);
 
-	// const handleGenreChange = (value: string) => {
-	//   setSelectedGenreID(value);
-	// };
+  // const handleGenreChange = (value: string) => {
+  //   setSelectedGenreID(value);
+  // };
 
-	const [searchKeyword, setSearchKeyword] = useState<string>("");
-	const [searchedMovies, setSearchedMovies] = useState<MovieDetails[]>([]);
+  // const [searchKeyword, setSearchKeyword] = useState<string>("");
+  const [searchedMovies, setSearchedMovies] = useState<MovieDetails[]>([]);
 
-	const handleSearchKeywordChange = (value: string) => {
-		console.log(value);
-		setSearchKeyword(value);
-	};
+  const handleSearchKeywordChange = async (value: string) => {
+    if (value) {
+      const searchedMovieData = await getMoviesByKeyword(value);
+      setSearchedMovies(searchedMovieData);
+    } else {
+      setSearchedMovies([]);
+    }
+  };
 
-	const handleSearchKeyDown = async (
-		event: React.KeyboardEvent<HTMLInputElement>
-	) => {
-		console.log("I am here", event.key);
-		if (event.key === "Enter") {
-			const searchedMovieData = await getMoviesByKeyword(searchKeyword);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const searchDebounceCallback = useCallback(
+    debounce(handleSearchKeywordChange, 500),
+    []
+  );
+  // const handleSearchKeyDown = async (
+  //   event: React.KeyboardEvent<HTMLInputElement>
+  // ) => {
+  //   if (event.key === "Enter") {
+  //     const searchedMovieData = await getMoviesByKeyword(searchKeyword);
+  //     setSearchedMovies(searchedMovieData);
+  //   }
+  // };
 
-			setSearchedMovies(searchedMovieData);
-		}
-	};
-
-	return (
-		<div className='App bg-slate-200'>
-			<Search
-				value={searchKeyword}
-				onChangeHandler={handleSearchKeywordChange}
-				onKeyDownHandler={handleSearchKeyDown}
-			/>
-			<div>
-				{searchedMovies.map((movie: MovieDetails) => (
-					<Card movieDetails={movie} />
-				))}
-			</div>
-		</div>
-	);
+  return (
+    <div className="App bg-slate-200">
+      <Search
+        onChangeHandler={searchDebounceCallback}
+        searchResults={searchedMovies}
+        //onKeyDownHandler={handleSearchKeyDown}
+      />
+      {/* <div>
+        {searchedMovies.map((movie: MovieDetails) => (
+          <React.Fragment key={movie.id}>
+            <Card movieDetails={movie} />
+          </React.Fragment>
+        ))}
+      </div> */}
+    </div>
+  );
 }
