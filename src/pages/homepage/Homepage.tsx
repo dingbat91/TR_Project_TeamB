4df2f797d607cc72a9c481e85796ea154b598f21
mrunalshri <mrunalyetekar@gmail.com>
2023-03-26 23:02:25 +0100

    fix:removed improper movies from popular list

--- src/pages/homepage/Homepage.tsx
+++ src/pages/homepage/Homepage.tsx
@@ -11,67 +11,68 @@ import { CardRow } from "../../Components/cardRow/CardRow";
 export const GenreContext = createContext<Genre[]>([]);
 
 export function Homepage() {
-	const [genrelist, setGenreList] = useState<Genre[]>([]);
-	const [popularMovies, setPopularMovies] = useState<Movie[]>([]);
+  const [genrelist, setGenreList] = useState<Genre[]>([]);
+  const [popularMovies, setPopularMovies] = useState<Movie[]>([]);
 
-	//Genre useEffect
-	useEffect(() => {
-		const fetchGenre = async () => {
-			const data = await APIFetch("/genre/movie/list");
-			setGenreList(data.genres);
-		};
-		fetchGenre();
-	}, []);
+  //Genre useEffect
+  useEffect(() => {
+    const fetchGenre = async () => {
+      const data = await APIFetch("/genre/movie/list");
+      setGenreList(data.genres);
+    };
+    fetchGenre();
+  }, []);
 
-	//Movie list useEffect
-	useEffect(() => {
-		const fetchRows = async () => {
-			const data = await APIFetch("/movie/popular", [
-				{ param: "adult", value: false },
-			]);
-			// const popdata: movieDetails[] = data.results.map((movie) => {
-			//   return { movieData: movie, genre: movie.genre_ids };
-			// });
-			setPopularMovies(data.results);
-		};
-		fetchRows();
-	}, []);
+  //Movie list useEffect
+  useEffect(() => {
+    const fetchRows = async () => {
+      const data = await APIFetch("/movie/popular", [
+        { param: "adult", value: false },
+        { param: "without_companies", value: "2740" }, // added to hide improper movie id 1087040
+      ]);
+      // const popdata: movieDetails[] = data.results.map((movie) => {
+      //   return { movieData: movie, genre: movie.genre_ids };
+      // });
+      setPopularMovies(data.results);
+    };
+    fetchRows();
+  }, []);
 
-	//   const [searchedMovies, setSearchedMovies] = useState<MovieDetails[]>([]);
+  //   const [searchedMovies, setSearchedMovies] = useState<MovieDetails[]>([]);
 
-	return (
-		<div className='App'>
-			{/* <div>
+  return (
+    <div className="App">
+      {/* <div>
         {searchedMovies.map((movie: MovieDetails) => (
           <Card movieDetails={movie} />
         ))}
       </div> */}
-			<GenreContext.Provider value={genrelist}>
-				<div className='card__netflixOriginal'>
-					<div className='card_title'>
-						<h2>Netflix Original</h2>
-					</div>
-					<div className='original__movies'>
-						<CardRow title='Netflix' movies={popularMovies} />
-					</div>
-				</div>
-				<div className='card__movies'>
-					<div className='movies__header'>
-						<h2>Trending Now</h2>
-					</div>
-					<div className='movies__container'>
-						<CardRow title='Trending' movies={popularMovies} />
-					</div>
-				</div>
-				<div className='card__movies'>
-					<div className='movies__header'>
-						<h2>Top Rated</h2>
-					</div>
-					<div className='movies__container'>
-						<CardRow title='top rated' movies={popularMovies} />
-					</div>
-				</div>
-			</GenreContext.Provider>
-		</div>
-	);
+      <GenreContext.Provider value={genrelist}>
+        <div className="card__netflixOriginal">
+          <div className="card_title">
+            <h2>Netflix Original</h2>
+          </div>
+          <div className="original__movies">
+            <CardRow title="Netflix" movies={popularMovies} />
+          </div>
+        </div>
+        <div className="card__movies">
+          <div className="movies__header">
+            <h2>Trending Now</h2>
+          </div>
+          <div className="movies__container">
+            <CardRow title="Trending" movies={popularMovies} />
+          </div>
+        </div>
+        <div className="card__movies">
+          <div className="movies__header">
+            <h2>Top Rated</h2>
+          </div>
+          <div className="movies__container">
+            <CardRow title="top rated" movies={popularMovies} />
+          </div>
+        </div>
+      </GenreContext.Provider>
+    </div>
+  );
 }
