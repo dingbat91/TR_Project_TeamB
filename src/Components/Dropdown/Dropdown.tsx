b2d7a2b12cd50aa8554165760bc2277b56e51898
Matthew Hanson <dingbat91@gmail.com>
2023-03-26 20:47:23 +0100

    Fixed for capitalisation

--- src/Components/Dropdown/Dropdown.tsx
+++ src/Components/Dropdown/Dropdown.tsx
@@ -2,42 +2,42 @@ import React from "react";
 import { DropdownProps } from "./type";
 
 const Dropdown: React.FC<DropdownProps> = ({
-  labelText,
-  name,
-  selectText,
-  value,
-  onChangeHandler,
-  options,
+	labelText,
+	name,
+	selectText,
+	value,
+	onChangeHandler,
+	options,
 }) => {
-  return (
-    <div>
-      {labelText && <label>{labelText} </label>}
-      <span>
-        <select
-          name={name}
-          aria-label="select-options"
-          className="select-Input"
-          defaultValue={selectText ? "" : value}
-          onChange={({ target }) => {
-            onChangeHandler(target.value);
-          }}
-        >
-          {selectText && (
-            <option disabled value="">
-              {selectText}
-            </option>
-          )}
-          {options.map((option, index) => {
-            return (
-              <React.Fragment key={`dropdown-${index}-${option}`}>
-                <option value={option.id}>{option.name}</option>
-              </React.Fragment>
-            );
-          })}
-        </select>
-      </span>
-    </div>
-  );
+	return (
+		<div>
+			{labelText && <label>{labelText} </label>}
+			<span>
+				<select
+					name={name}
+					aria-label='select-options'
+					className='select-Input'
+					defaultValue={selectText ? "" : value}
+					onChange={({ target }) => {
+						onChangeHandler(target.value);
+					}}
+				>
+					{selectText && (
+						<option disabled value=''>
+							{selectText}
+						</option>
+					)}
+					{options.map((option, index) => {
+						return (
+							<React.Fragment key={`dropdown-${index}-${option}`}>
+								<option value={option.id}>{option.name}</option>
+							</React.Fragment>
+						);
+					})}
+				</select>
+			</span>
+		</div>
+	);
 };
 
 export default Dropdown;
