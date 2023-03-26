9d21d38a847be5bb77ec101e746d051051e0e3bd
mrunalshri <mrunalyetekar@gmail.com>
2023-03-26 19:23:45 +0100

    feat: added watch list provider in layout

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,31 +1,15 @@
-import React, {
-  createContext,
-  Dispatch,
-  SetStateAction,
-  useContext,
-  useState,
-} from "react";
+import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header/header";
-import { MovieDetails } from "../types/movie";
-
-export const WatchlistContext = createContext<MovieDetails[]>([]);
-export const UpdateWatchlistContext = createContext<
-  Dispatch<SetStateAction<MovieDetails[]>>
->(() => null);
+import { WatchListProvider } from "../watchList_context";
 
 export const Layout: React.FC = () => {
-  const [watchlistedMovies, setWatchlistedMovies] = useState<MovieDetails[]>(
-    []
-  );
   return (
     <>
       <Header />
-      <WatchlistContext.Provider value={watchlistedMovies}>
-        <UpdateWatchlistContext.Provider value={setWatchlistedMovies}>
-          <Outlet />
-        </UpdateWatchlistContext.Provider>
-      </WatchlistContext.Provider>
+      <WatchListProvider>
+        <Outlet />
+      </WatchListProvider>
     </>
   );
 };
