327a25913e9d88d6bb6aec9b0d9331b19bf03f45
mrunalshri <mrunalyetekar@gmail.com>
2023-03-26 19:22:52 +0100

    feat: craeted watchlist page and context

--- src/Components/watchlist/watchlist.tsx
+++ src/Components/watchlist/watchlist.tsx
@@ -1,20 +1,35 @@
-import React, { useContext } from "react";
-import { WatchlistContext } from "../../layout/layout";
+import React from "react";
+import { useWatchListContext } from "../../watchList_context";
 
 const Watchlist: React.FC = () => {
-  const watchlistMoviesContext = useContext(WatchlistContext);
+  const { watchlistedMovies, handleWatchListClick } = useWatchListContext();
+
   return (
     <>
-      <div className="text-center text-2xl pb-2.5">My WatchList</div>
+      <div className="text-center text-2xl pb-2.5">My Watchlist</div>
       <div className="flex flex-wrap justify-center">
-        {watchlistMoviesContext.length > 0 ? (
-          watchlistMoviesContext.map((movie) => (
-            <div className="basis-1/6 m-1" key={`watchlist-${movie.id}`}>
+        {watchlistedMovies.length > 0 ? (
+          watchlistedMovies.map((movie) => (
+            <div
+              className="flex flex-col items-center basis-1/6 m-1"
+              key={`watchlist-${movie.id}`}
+            >
               <img
                 className="md:w-56"
                 src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
                 alt={`${movie.original_title} movie poster`}
               />
+              <button
+                onClick={() => {
+                  handleWatchListClick(movie);
+                }}
+                className="relative inline-flex items-center justify-start px-6 py-2 overflow-hidden font-medium transition-all bg-white hover:bg-white group w-full"
+              >
+                <span className="w-56 h-48 rounded rotate-[-40deg] bg-red-700 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+                <span className="relative w-full text-center text-black transition-colors duration-300 ease-in-out group-hover:text-white">
+                  Remove
+                </span>
+              </button>
             </div>
           ))
         ) : (
