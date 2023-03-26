fbb968992862de3730be40f76a933945314d6646
Matthew Hanson <dingbat91@gmail.com>
2023-03-26 19:40:34 +0100

    Tests for ActorPage

--- src/scripts/fetch/testing/mswConfig/handlers.ts
+++ src/scripts/fetch/testing/mswConfig/handlers.ts
@@ -1,28 +1,64 @@
 import { rest } from "msw";
 import {
-  movieID550DetailsMockResponse,
-  movieID603692DetailsMockResponse,
-  personID287DetailsMockResponse,
-  searchByPersonMockResponse,
+	movieID420818VideoMockResponse,
+	movieID550DetailsMockResponse,
+	movieID603692DetailsMockResponse,
+	personID119589DetailsMockResponse,
+	personID119589PopMoviesMockResponse,
+	personID119589SocialMediaMockResponse,
+	personID287DetailsMockResponse,
+	searchByPersonMockResponse,
 } from "./mock";
 
 //base movie URL to C&P for new handlers: https://api.themoviedb.org/3/
 
 export const handlers = [
-  rest.get("https://api.themoviedb.org/3/movie/550", (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(movieID550DetailsMockResponse));
-  }),
-  rest.get("https://api.themoviedb.org/3/movie/603692", (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(movieID603692DetailsMockResponse));
-  }),
-  rest.get("https://api.themoviedb.org/3/person/287", (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(personID287DetailsMockResponse));
-  }),
-  rest.get("https://api.themoviedb.org/3/search/person", (req, res, ctx) => {
-    const query = req.url.searchParams.get("query");
+	rest.get("https://api.themoviedb.org/3/movie/550", (req, res, ctx) => {
+		return res(ctx.status(200), ctx.json(movieID550DetailsMockResponse));
+	}),
+	rest.get("https://api.themoviedb.org/3/movie/603692", (req, res, ctx) => {
+		return res(ctx.status(200), ctx.json(movieID603692DetailsMockResponse));
+	}),
+	rest.get("https://api.themoviedb.org/3/person/287", (req, res, ctx) => {
+		return res(ctx.status(200), ctx.json(personID287DetailsMockResponse));
+	}),
+	rest.get("https://api.themoviedb.org/3/search/person", (req, res, ctx) => {
+		const query = req.url.searchParams.get("query");
 
-    if (query === "Bradley") {
-      return res(ctx.status(200), ctx.json(searchByPersonMockResponse));
-    }
-  }),
+		if (query === "Bradley") {
+			return res(ctx.status(200), ctx.json(searchByPersonMockResponse));
+		}
+	}),
+
+	rest.get(
+		"https://api.themoviedb.org/3/person/71375/external_ids",
+		(req, res, ctx) => {
+			return res(
+				ctx.status(200),
+				ctx.json(personID119589SocialMediaMockResponse)
+			);
+		}
+	),
+	rest.get(
+		"https://api.themoviedb.org/3/person/119589?language=en-GB&api_key=0d24e00bc514e51eb0f40d8e487c2fc9",
+		(req, res, ctx) => {
+			return res(ctx.status(200), ctx.json(personID119589DetailsMockResponse));
+		}
+	),
+	rest.get(
+		"https://api.themoviedb.org/3/discover/movie?api_key=0d24e00bc514e51eb0f40d8e487c2fc9&sort_by=popularity.desc&with_cast=119589",
+		(req, res, ctx) => {
+			return res(
+				ctx.status(200),
+				ctx.json(personID119589PopMoviesMockResponse)
+			);
+		}
+	),
+
+	rest.get(
+		"https://api.themoviedb.org/3/movie/420818/videos?api_key=0d24e00bc514e51eb0f40d8e487c2fc9",
+		(req, res, ctx) => {
+			return res(ctx.status(200), ctx.json(movieID420818VideoMockResponse));
+		}
+	),
 ];
