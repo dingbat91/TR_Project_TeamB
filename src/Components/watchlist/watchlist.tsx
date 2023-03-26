6aae27c727b667000ec08e4e57ec8efc673c30ab
mrunalshri <mrunalyetekar@gmail.com>
2023-03-26 19:41:06 +0100

    fix: modified styling for remove button

--- src/Components/watchlist/watchlist.tsx
+++ src/Components/watchlist/watchlist.tsx
@@ -19,17 +19,28 @@ const Watchlist: React.FC = () => {
                 src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
                 alt={`${movie.original_title} movie poster`}
               />
-              <button
+              <span
                 onClick={() => {
                   handleWatchListClick(movie);
                 }}
-                className="relative inline-flex items-center justify-start px-6 py-2 overflow-hidden font-medium transition-all bg-white hover:bg-white group w-full"
+                className="cursor-pointer flex justify-center my-2 text-black py-1 md:w-56 w-56 bg-slate-200"
               >
-                <span className="w-56 h-48 rounded rotate-[-40deg] bg-red-700 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
-                <span className="relative w-full text-center text-black transition-colors duration-300 ease-in-out group-hover:text-white">
-                  Remove
-                </span>
-              </button>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                  />
+                </svg>
+                <span className="ml-1 text-base">Remove</span>
+              </span>
             </div>
           ))
         ) : (
