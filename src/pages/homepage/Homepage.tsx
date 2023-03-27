dbfcf6d85d4aa09c5ca42ab1c2fe8aa5c8c6a916
Jonald Damaso <damasojonald@gmail.com>
2023-03-27 16:31:41 +0100

    Feat: Homepage added dynamic data per row
    Co-authored-by: Matthew Hanson <dingbat91@users.noreply.github.com>

--- src/pages/homepage/Homepage.tsx
+++ src/pages/homepage/Homepage.tsx
@@ -1,18 +1,31 @@
 import React, { createContext, useEffect, useState } from "react";
-// import "./Homepage.css";
+import "./Homepage.css";
 import Card from "../../Components/card/card";
 import { getMoviesByKeyword } from "../../scripts/services/service";
-import { Movie, MovieDetails, PopularMoviesResponse } from "../../types/movie";
+import {
+	PopularMoviesResponse,
+	TopRatedMovie,
+	NowPlayingMoviesResponse,
+	UpcomingMoviesResponse,
+	MovieDetails
+  } from "../../types/movie";
 import { Genre } from "../../types/genres";
 import { APIFetch } from "../../scripts/fetch/fetch";
 import { CardRow } from "../../Components/cardRow/CardRow";
-// import { movieDetails } from "../../components/cardRow/CardRow";
+import { Movie } from "../../types/movie";
+
+export interface movieDetails {
+	  movieData: Movie[];
+	}
 
 export const GenreContext = createContext<Genre[]>([]);
 
 export function Homepage() {
 	const [genrelist, setGenreList] = useState<Genre[]>([]);
 	const [popularMovies, setPopularMovies] = useState<Movie[]>([]);
+	const [topratedMovies, setTopRatedMovies] = useState<Movie[]>([]);
+	const [nowplayingdMovies, setNowPlayingMovies] = useState<Movie[]>([]);
+	const [upcomingMovies, setUpcomingMovies] = useState<Movie[]>([]);
 
 	//Genre useEffect
 	useEffect(() => {
@@ -26,13 +39,29 @@ export function Homepage() {
 	//Movie list useEffect
 	useEffect(() => {
 		const fetchRows = async () => {
-			const data = await APIFetch("/movie/popular", [
-				{ param: "adult", value: false },
-			]);
-			// const popdata: movieDetails[] = data.results.map((movie) => {
-			//   return { movieData: movie, genre: movie.genre_ids };
-			// });
-			setPopularMovies(data.results);
+			// const data = await APIFetch("/movie/popular", [
+			// 	{ param: "adult", value: false },
+			// ]);
+			// // const popdata: movieDetails[] = data.results.map((movie) => {
+			// //   return { movieData: movie, genre: movie.genre_ids };
+			// // });
+			// setPopularMovies(data.results);
+			
+			const dataupcoming = (await APIFetch("/movie/upcoming")) as UpcomingMoviesResponse;
+			const upcomingdata:Movie[] = dataupcoming.results
+			setUpcomingMovies(upcomingdata);
+
+			const datanowplaying = (await APIFetch("/movie/now_playing")) as NowPlayingMoviesResponse;
+			const nowdata:Movie[] = datanowplaying.results
+			setNowPlayingMovies(nowdata);
+
+			const datapop = (await APIFetch("/movie/popular")) as PopularMoviesResponse;
+			const popdata:Movie[] = datapop.results 
+			setPopularMovies(popdata);
+
+			const datatoprated = (await APIFetch("/movie/top_rated")) as TopRatedMovie;
+			const toprateddata: Movie[] = datatoprated.results
+			setTopRatedMovies(toprateddata);
 		};
 		fetchRows();
 	}, []);
@@ -57,10 +86,10 @@ export function Homepage() {
 				</div>
 				<div className='card__movies'>
 					<div className='movies__header'>
-						<h2>Trending Now</h2>
+						<h2>Now Playing</h2>
 					</div>
 					<div className='movies__container'>
-						<CardRow title='Trending' movies={popularMovies} />
+						<CardRow title='Trending' movies={nowplayingdMovies} />
 					</div>
 				</div>
 				<div className='card__movies'>
@@ -68,7 +97,15 @@ export function Homepage() {
 						<h2>Top Rated</h2>
 					</div>
 					<div className='movies__container'>
-						<CardRow title='top rated' movies={popularMovies} />
+						<CardRow title='top rated' movies={topratedMovies} />
+					</div>
+				</div>
+				<div className='card__movies'>
+					<div className='movies__header'>
+						<h2>Upcoming Movies</h2>
+					</div>
+					<div className='movies__container'>
+						<CardRow title='top rated' movies={upcomingMovies} />
 					</div>
 				</div>
 			</GenreContext.Provider>
