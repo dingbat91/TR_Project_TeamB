c944b48439cef58212512bfcabd4b246200990d2
Matthew Hanson <dingbat91@gmail.com>
2023-03-28 19:36:25 +0100

    removal of console.log

--- src/Components/cardRow/CardRow.tsx
+++ src/Components/cardRow/CardRow.tsx
@@ -11,38 +11,35 @@ import "./CardRow.css";
 // }
 
 interface CardRowInterface {
-  movies: Movie[];
-  title: string;
+	movies: Movie[];
+	title: string;
 }
 
 export const CardRow: React.FC<CardRowInterface> = ({ movies, title }) => {
-  const [filter, setFilter] = useState<string>("A"); // 28
-  const Genres = useContext(GenreContext);
+	const [filter, setFilter] = useState<string>("A"); // 28
+	const Genres = useContext(GenreContext);
 
-  console.log(filter);
-  console.log(Genres);
-
-  return (
-    <>
-      {/* <Dropdown
+	return (
+		<>
+			{/* <Dropdown
 				options={Genres}
 				name='RowLabel'
 				value=''
 				onChangeHandler={setFilter}
 			/> */}
 
-      <>
-        {movies.map((movie: Movie) => (
-          <React.Fragment key={`movie-data-${movie.id}`}>
-            {/* added extra check for inappropriate movie poster id: 1087040 */}
-            {movie.id !== 1087040 &&
-              (movie.genre_ids.includes(parseInt(filter)) ||
-                filter === "A") && (
-                <MovieData movieDetails={movie} title={title} />
-              )}
-          </React.Fragment>
-        ))}
-      </>
-    </>
-  );
+			<>
+				{movies.map((movie: Movie) => (
+					<React.Fragment key={`movie-data-${movie.id}`}>
+						{/* added extra check for inappropriate movie poster id: 1087040 */}
+						{movie.id !== 1087040 &&
+							(movie.genre_ids.includes(parseInt(filter)) ||
+								filter === "A") && (
+								<MovieData movieDetails={movie} title={title} />
+							)}
+					</React.Fragment>
+				))}
+			</>
+		</>
+	);
 };
